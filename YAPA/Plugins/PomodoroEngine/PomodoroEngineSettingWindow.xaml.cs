b51c3ad12fb227449c8d900843d8d69abce3a8b0
Floatas <floatas@outlook.com>
2017-02-05 14:32:23 +0200

    UI improvements

--- YAPA/Plugins/PomodoroEngine/PomodoroEngineSettingWindow.xaml.cs
+++ YAPA/Plugins/PomodoroEngine/PomodoroEngineSettingWindow.xaml.cs
@@ -1,17 +1,26 @@
-﻿using System.Windows.Controls;
+﻿using System.Linq;
+using System.Windows.Controls;
 using YAPA.Shared;
 
 namespace YAPA.WPF
 {
-    /// <summary>
-    /// Interaction logic for PomodoroEngineSettingWindow.xaml
-    /// </summary>
     public partial class PomodoroEngineSettingWindow : UserControl
     {
         public PomodoroEngineSettingWindow(PomodoroEngineSettings settings)
         {
             settings.DeferChanges();
             InitializeComponent();
+
+            //HACK!
+            Enumerable.Range(1,60).Select(x =>
+            {
+                WorkTimeSelect.Items.Add(x);
+                BreakTimeSelect.Items.Add(x);
+                LongBreakTimeSelect.Items.Add(x);
+
+                return true;
+            }).ToList();
+
             DataContext = settings;
         }
     }
