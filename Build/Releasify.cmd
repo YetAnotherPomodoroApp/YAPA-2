ed87bfe64ece2dc38ef2c0b91c0443bc0a26536c
Floatas <floatas@outlook.com>
2017-02-06 20:56:59 +0200

    add --no-msi

--- Build/Releasify.cmd
+++ Build/Releasify.cmd
@@ -1,3 +1,3 @@
 set version=%1
 set releaseDir=%2
-..\packages\squirrel.windows.1.5.2\tools\squirrel --releasify YAPA2.%version%.nupkg --releaseDir=%releaseDir%
+..\packages\squirrel.windows.1.5.2\tools\squirrel --releasify YAPA2.%version%.nupkg --releaseDir=%releaseDir% --no-msi
