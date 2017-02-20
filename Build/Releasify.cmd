551dc557db0d9e688e057c24ee687987f880c781
Floatas <floatas@outlook.com>
2017-02-20 23:33:59 +0200

    fixed local squirrel path

--- Build/Releasify.cmd
+++ Build/Releasify.cmd
@@ -4,4 +4,4 @@ set version=%2
 if "%version%" == "" (
    set version=%BUILD_BUILDNUMBER%
 )
-squirrel\tools\squirrel --releasify YAPA2.%version%.nupkg --releaseDir=%releaseDir% --previous-releases-url=ftp://s1.floatas.net/YAPA-2 --no-msi
+squirrel\update --releasify YAPA2.%version%.nupkg --releaseDir=%releaseDir% --previous-releases-url=ftp://s1.floatas.net/YAPA-2 --no-msi
