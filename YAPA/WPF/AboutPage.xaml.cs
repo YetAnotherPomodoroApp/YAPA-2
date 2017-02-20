ec9ac985f028d8fac7ee1e5c40359cca65a0fb0e
Floatas <floatas@outlook.com>
2017-02-20 19:17:10 +0200

    simple about page

--- YAPA/WPF/AboutPage.xaml.cs
+++ YAPA/WPF/AboutPage.xaml.cs
@@ -1,28 +1,20 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System.Diagnostics;
 using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Data;
 using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 
 namespace YAPA.WPF
 {
-    /// <summary>
-    /// Interaction logic for AboutPage.xaml
-    /// </summary>
     public partial class AboutPage : UserControl
     {
         public AboutPage()
         {
             InitializeComponent();
+        }
+
+        private void Hyperlink_Click(object sender, RoutedEventArgs e)
+        {
+            Process.Start(((Hyperlink)sender).NavigateUri.ToString());
         }
     }
 }
