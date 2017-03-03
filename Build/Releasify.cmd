c0939a200d1162e968ce5d25bfa1d557152b471c
Floatas <floatas@outlook.com>
2017-03-03 20:41:12 +0200

    Use custom squirrel nuget

--- Build/Releasify.cmd
+++ Build/Releasify.cmd
@@ -4,4 +4,4 @@ set version=%2
 if "%version%" == "" (
    set version=%BUILD_BUILDNUMBER%
 )
-squirrel\update --releasify YAPA2.%version%.nupkg --releaseDir=%releaseDir% --previous-releases-url=ftp://s1.floatas.net/YAPA-2 --no-msi
+..\packages\squirrel.windows.1.5.28\tools\Squirrel --releasify YAPA2.%version%.nupkg --releaseDir=%releaseDir% --previous-releases-url=ftp://s1.floatas.net/YAPA-2 --no-msi
