321c6853bb77ca44435ccd5e02353a53ca11f8d8
Floatas <floatas@outlook.com>
2017-06-06 22:08:20 +0300

    Unit tests:  auto start break

--- YAPA.Shared.Tests/TestHelpers.cs
+++ YAPA.Shared.Tests/TestHelpers.cs
@@ -1,4 +1,7 @@
 ﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
 using YAPA.Contracts;
 using YAPA.Shared.Contracts;
 
@@ -31,4 +34,28 @@ namespace YAPA.Shared.Tests
             return DateToReturn;
         }
     }
+
+    public static class Timeout
+    {
+        public static async Task WaitPhaseChange(PomodoroEngine engine, PomodoroPhase phase)
+        {
+            var timeout = TimeSpan.FromSeconds(5);
+            var task = Task.Run(() =>
+                {
+                    while (true)
+                    {
+                        if (engine.Phase == phase)
+                            break;
+
+                        Thread.Sleep(50);
+                    }
+                });
+
+            if (await Task.WhenAny(task, Task.Delay(timeout)) != task)
+            {
+                Assert.Fail($"Timout: Phase didn't change to {phase}, actual phase:{engine.Phase}");
+            }
+        }
+    }
+
 }
