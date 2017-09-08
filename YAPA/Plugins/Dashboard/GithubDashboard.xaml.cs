5e802af7332ff68b5c435de00042d477eed199a6
Floatas <floatas@outlook.com>
2017-09-08 19:17:35 +0300

    Fixed dashboad loading, now it's really async

--- YAPA/Plugins/Dashboard/GithubDashboard.xaml.cs
+++ YAPA/Plugins/Dashboard/GithubDashboard.xaml.cs
@@ -3,15 +3,11 @@ using System.Globalization;
 using System.Linq;
 using System.Threading.Tasks;
 using System.Windows;
-using System.Windows.Controls;
 using YAPA.WPF;
 
 namespace YAPA.Plugins.Dashboard
 {
-    /// <summary>
-    /// Interaction logic for Dashboard.xaml
-    /// </summary>
-    public partial class GithubDashboard : UserControl
+    public partial class GithubDashboard
     {
         private readonly Shared.Common.Dashboard _dashboard;
 
@@ -19,54 +15,55 @@ namespace YAPA.Plugins.Dashboard
         {
             _dashboard = dashboard;
 
-            this.Loaded += Dashboard_Loaded;
+            Loaded += Dashboard_Loaded;
 
             InitializeComponent();
         }
 
-        private async void Dashboard_Loaded(object sender, System.Windows.RoutedEventArgs e)
+        private void Dashboard_Loaded(object sender, RoutedEventArgs e)
         {
-            await Task.Run(() =>
-            {
+            Task.Run(() =>
+           {
+               var dfi = DateTimeFormatInfo.CurrentInfo;
+               var cal = dfi?.Calendar;
 
-                var dfi = DateTimeFormatInfo.CurrentInfo;
-                var cal = dfi.Calendar;
+               if (cal == null)
+               {
+                   return;
+               }
 
+               var pomodoros =
+                   _dashboard.GetPomodoros()
+                       .Select(
+                           x =>
+                               new
+                               {
+                                   week = cal.GetWeekOfYear(x.DateTime, CalendarWeekRule.FirstFullWeek, dfi.FirstDayOfWeek),
+                                   month = cal.GetMonth(x.DateTime),
+                                   x
+                               })
+                       .ToList();
+               var max = pomodoros.Max(x => x.x.Count);
 
-                var pomodoros =
-                    _dashboard.GetPomodoros()
-                        .Select(
-                            x =>
-                                new
-                                {
-                                    week = cal.GetWeekOfYear(x.DateTime, CalendarWeekRule.FirstFullWeek, dfi.FirstDayOfWeek),
-                                    month = cal.GetMonth(x.DateTime),
-                                    x
-                                })
-                        .ToList();
-                var max = pomodoros.Max(x => x.x.Count);
+               foreach (var pomodoro in pomodoros.GroupBy(x => x.month))
+               {
+                   var month = pomodoro.Select(x => x.x.ToPomodoroViewModel(x.week, GetLevelFromCount(x.x.Count, max)));
+                   Dispatcher.Invoke(() =>
+                   {
+                       WeekStackPanel.Children.Add(new PomodoroMonth(month));
+                   });
+               }
 
-                foreach (var pomodoro in pomodoros.GroupBy(x => x.month))
-                {
-                    var month = pomodoro.Select(x => x.x.ToPomodoroViewModel(x.week, GetLevelFromCount(x.x.Count, max)));
-                    Dispatcher.Invoke(() =>
-                    {
-                        WeekStackPanel.Children.Add(new PomodoroMonth(month));
-                    });
-                }
+               Dispatcher.Invoke(() =>
+               {
+                   var weekShift = DayOfWeek.Monday - CultureInfo.CurrentCulture.DateTimeFormat.FirstDayOfWeek;
+                   MondayTextBlock.Margin = new Thickness(0, 12 * weekShift - 1, 0, 12);
 
+                   DayPanel.Visibility = Visibility.Visible;
+                   LoadingPanel.Visibility = Visibility.Collapsed;
+               });
 
-
-                Dispatcher.Invoke(() =>
-                {
-                    var weekShift = DayOfWeek.Monday - CultureInfo.CurrentCulture.DateTimeFormat.FirstDayOfWeek;
-                    MondayTextBlock.Margin = new Thickness(0, 12 * weekShift - 1, 0, 12);
-
-                    DayPanel.Visibility = Visibility.Visible;
-                    LoadingPanel.Visibility = Visibility.Collapsed;
-                });
-
-            });
+           });
         }
 
         private PomodoroLevelEnum GetLevelFromCount(int count, int maxCount)
@@ -79,21 +76,24 @@ namespace YAPA.Plugins.Dashboard
             {
                 return PomodoroLevelEnum.Level4;
             }
+
             var level = (double)count / maxCount;
+            var displayLevel = PomodoroLevelEnum.Level4;
+
             if (level < 0.25)
             {
-                return PomodoroLevelEnum.Level1;
+                displayLevel = PomodoroLevelEnum.Level1;
             }
             else if (level < 0.50)
             {
-                return PomodoroLevelEnum.Level2;
+                displayLevel = PomodoroLevelEnum.Level2;
             }
             else if (level < 0.75)
             {
-                return PomodoroLevelEnum.Level3;
+                displayLevel = PomodoroLevelEnum.Level3;
             }
 
-            return PomodoroLevelEnum.Level4;
+            return displayLevel;
         }
     }
 
