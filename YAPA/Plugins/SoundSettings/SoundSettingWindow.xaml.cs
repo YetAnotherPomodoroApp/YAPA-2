469769b4fbc43d83041d5759b73a98b4fd11c9d1
Floatas <floatas@outlook.com>
2017-10-07 09:36:02 +0300

    all sound settings moved to sound tab

--- YAPA/Plugins/SoundSettings/SoundSettingWindow.xaml.cs
+++ YAPA/Plugins/SoundSettings/SoundSettingWindow.xaml.cs
@@ -1,14 +1,20 @@
-﻿using YAPA.Plugins.SoundSettings.SoundNotifications;
+﻿using YAPA.Plugins.SoundSettings.MusicPlayer;
+using YAPA.Plugins.SoundSettings.SoundNotifications;
+using YAPA.Shared.Common;
 
 namespace YAPA.Plugins.SoundSettings
 {
     public partial class SoundSettingWindow
     {
-        public SoundSettingWindow(SoundNotificationSettingWindow soundNotification, MusicPlayer.MusicPlayerPluginSettingWindow musicPlayer)
+        public SoundSettingWindow(SoundNotificationSettingWindow soundNotification, MusicPlayerPluginSettingWindow musicPlayer, PomodoroEngineSettings engineSettings)
         {
             InitializeComponent();
             Container.Children.Add(soundNotification);
             Container.Children.Add(musicPlayer);
+
+            engineSettings.DeferChanges();
+
+            DataContext = engineSettings;
         }
     }
 }
