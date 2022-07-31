f59d0aab57517b2839863b4bd179f61b9d6c0e4f
Šarūnas <floatas@outlook.com>
2022-07-31 19:50:53 +0300

    task preview window + follow timer

--- YAPA/Plugins/Tasks/TasksMeta.cs
+++ YAPA/Plugins/Tasks/TasksMeta.cs
@@ -4,6 +4,7 @@ using System.Collections.Generic;
 using System.Collections.ObjectModel;
 using System.ComponentModel;
 using System.Runtime.CompilerServices;
+using System.Windows;
 using YAPA.Shared.Contracts;
 
 namespace YAPA.Plugins.Tasks
@@ -26,7 +27,24 @@ namespace YAPA.Plugins.Tasks
 
     public class TasksPlugin : IPlugin
     {
+        Window _appWindow;
+        Window _taskPreview;
+        public TasksPlugin(IApplication app)
+        {
+            _taskPreview = new TasksPreview();
+            _taskPreview.Visibility = Visibility.Visible;
+            _taskPreview.WindowStyle = WindowStyle.None;
+            _taskPreview.ResizeMode = ResizeMode.NoResize;
+            _appWindow = (Window)app;
+            _appWindow.LocationChanged += Window_LocationChanged;
+        }
 
+        private void Window_LocationChanged(object sender, EventArgs e)
+        {
+            Console.WriteLine(_appWindow.Left + " " + _appWindow.Top);
+            _taskPreview.Left = _appWindow.Left; 
+            _taskPreview.Top = _appWindow.Top + _appWindow.Height;
+        }
     }
 
     public class TaskGroup : INotifyPropertyChanged
---
+++ YAPA/Plugins/Tasks/TasksPreview.xaml.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace YAPA.Plugins.Tasks
+{
+    /// <summary>
+    /// Interaction logic for TasksPreview.xaml
+    /// </summary>
+    public partial class TasksPreview : Window
+    {
+        public TasksPreview()
+        {
+            Topmost = true;
+            DataContext = this;
+            InitializeComponent();
+        }
+
+        public double ClockOpacity => 1;
+
+        public Brush TextBrush
+        {
+            get
+            {
+                return new SolidColorBrush(Colors.Red);
+            }
+        }
+
+    }
+}
